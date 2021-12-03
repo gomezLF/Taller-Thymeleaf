6109eb3d299e8bd6889da5b07d2c04aa244345bb
Felipe Gómez <luis.andrade18@hotmail.com>
2021-12-03 16:53:16 -0500

    Arreglado el método de editar un Salesorderdetail

--- src/main/java/co/edu/icesi/services/SalesorderdetailServiceImp.java
+++ src/main/java/co/edu/icesi/services/SalesorderdetailServiceImp.java
@@ -1,13 +1,10 @@
 package co.edu.icesi.services;
 
-import java.math.BigDecimal;
 import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
-import co.edu.icesi.exception.LogicalException;
 import co.edu.icesi.model.sales.Salesorderdetail;
-import co.edu.icesi.model.sales.SalesorderdetailPK;
 import co.edu.icesi.repositories.SalesorderdetailRepo;
 import co.edu.icesi.repositories.SalesorderheaderRepo;
 
@@ -40,7 +37,7 @@ public class SalesorderdetailServiceImp implements SalesorderdetailService {
 
 	@Override
 	public void editSalesOrderDetails(Salesorderdetail salesorderdetail) {
-		Optional<Salesorderdetail> temp = findSalesorderdetail(salesorderdetail.getId().getSalesorderdetailid());
+		Optional<Salesorderdetail> temp = findSalesorderdetail(salesorderdetail.getId());
 		
 		if(temp.isPresent()) {
 			temp.get().setCarriertrackingnumber(salesorderdetail.getCarriertrackingnumber());
@@ -64,28 +61,6 @@ public class SalesorderdetailServiceImp implements SalesorderdetailService {
 	@Override
 	public Optional<Salesorderdetail> findSalesorderdetail(int id) {
 		return salesorderdetailRepo.findById(id);
-	}
-	
-	private void validateConstraints(Salesorderdetail salesorderdetail) throws LogicalException{
-		if(salesorderdetail.getUnitprice().compareTo(BigDecimal.ZERO) <= 0) {
-			throw new LogicalException("El precio unitario de los detalles de la orden es menor o igual a cero.");
-			
-		}else if(salesorderdetail.getUnitprice().compareTo(BigDecimal.ZERO) < 0) {
-			throw new LogicalException("El descuento unitario de los detalles de la orden es menor a cero"); 
-			
-		}else if(calculateStandardPrice(salesorderdetail.getUnitprice(), salesorderdetail.getOrderqty()).compareTo(BigDecimal.ZERO) <= 0) {
-			throw new LogicalException("El precio estandar de los detalles de la orden es menor o igual a cero");
-		}
-	}
-	
-	private BigDecimal calculateStandardPrice(BigDecimal unitprice, int orderqty) {
-		BigDecimal totalcost = BigDecimal.ZERO;
-		BigDecimal itemcost = BigDecimal.ZERO;
-		
-		itemcost = unitprice.multiply(BigDecimal.valueOf(orderqty));
-		totalcost = totalcost.add(itemcost);
-		
-		return totalcost;
 	}
 
 	@Override
