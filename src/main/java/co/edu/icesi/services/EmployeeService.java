4b43760dd3699f80abe95822e5eb2c37d28f2da5
Felipe Gómez <luis.andrade18@hotmail.com>
2021-12-03 17:43:45 -0500

    Creada la interfaz del servicio de Employee con sus respectivos metodos

---
+++ src/main/java/co/edu/icesi/services/EmployeeService.java
@@ -0,0 +1,15 @@
+package co.edu.icesi.services;
+
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import co.edu.icesi.model.hr.Employee;
+
+@Service
+public interface EmployeeService {
+	void saveEmployee(Employee employee);
+	void editEmployee(Employee employee);
+	Optional<Employee> findEmployee(int id);
+	Iterable<Employee> findAll();
+}
