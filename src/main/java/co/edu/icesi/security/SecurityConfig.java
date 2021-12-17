ebc83758a7343c9fa39e3e284e19b241df272e17
Felipe Gómez <luis.andrade18@hotmail.com>
2021-12-16 23:05:43 -0500

    Arreglado el problema de los permisos de usuarios

--- src/main/java/co/edu/icesi/security/SecurityConfig.java
+++ src/main/java/co/edu/icesi/security/SecurityConfig.java
@@ -1,6 +1,8 @@
 package co.edu.icesi.security;
 
+import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
@@ -8,18 +10,18 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import co.edu.icesi.model.person.UserType;
 
 
-
+@Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	
 	@Autowired
     private LoggingAccessDeniedHandler accessDeniedHandler;
 	
 	@Override
-    protected void configure(HttpSecurity httpSecurity) throws Exception {
+    protected void configure(@NotNull HttpSecurity httpSecurity) throws Exception {
 	 	
 	 	httpSecurity.formLogin().loginPage("/login").permitAll().and().authorizeRequests()
  		.antMatchers("/salesperson/**").hasRole(UserType.ADMINISTRATIOR.toString())
- 		.antMatchers("/creditcard/**", "/creditcard/**", "/salesordersheader/**", "/salesorderdetail/**").hasRole(UserType.OPERATOR.toString())
+ 		.antMatchers("/creditcard/**", "/salesorderheader/**", "/salesorderdetail/**").hasRole(UserType.OPERATOR.toString())
  		.anyRequest().authenticated().and()
 		.httpBasic().and().logout().invalidateHttpSession(true)
 		.clearAuthentication(true)
